418f9c35696999a2f49a3cec1571f4a21223ecb2
HR_SONG <yooatom2200@gmail.com>
2021-02-11 13:13:37 +0900

    deque 추가

--- 백준_3190.py
+++ 백준_3190.py
@@ -1,23 +1,26 @@
+from collections import deque
+
 #뱀. 머리좌표, 꼬리좌표
 hx, hy = 0, 0
 tx, ty = 0, 0
+snake = deque([[hx, hy]])
 turn = "r"
 
 #보드크기, 뱀 = 1, 사과 = 2. 빈공간 = 0
 N = int(input())
-board = [[0 for i in range (N)] for i in range (N)]
-board[0][0] = 1
+board = [[0 for i in range (N)] for j in range (N)]
+board[hx][hy] = 1
 
 #사과개수, 위치설정
 apple = int(input())
 for i in range(apple):
     aX, aY = map(int, input().split())
-    board[aX][aY] = 2
+    board[aX-1][aY-1] = 2
 
 #뱀 방향변환 정보
 L = int(input())
-tX = [L]
-tC = [L]
+tX = [0 for _ in range(L)]
+tC = [0 for _ in range(L)]
 for i in range(L):
     tmp = input().split()
     tX[i] = int(tmp[0])
@@ -27,22 +30,33 @@ time = 1
 while(True):
     #머리이동
     if turn == "r":
-        hx = hx + 1
-    elif turn == "l":
-        hx = hx - 1
-    elif turn == "u":
-        hy = hy - 1
-    else:
         hy = hy + 1
+    elif turn == "l":
+        hy = hy - 1
+    elif turn == "u":
+        hx = hx - 1
+    else:
+        hx = hx + 1
 
     #머리 위치한곳 확인(빈곳 or 사과 or 몸통)
-    if board[hx][hy] == 0:
+    if board[hx][hy]  == 0:
         board[hx][hy] = 1
+        snake.append([hx,hy])
+        tx, ty = snake.popleft()
         board[tx][ty] = 0
     elif board[hx][hy] == 2:
         board[hx][hy] = 1
+        snake.append([hx,hy])
     else:
         break
+
+    #머리가 보드 밖으로 진출시
+    if not 0 <= hx < N or not 0 <= hy < N:
+        break
+    
+    print("--------", time)
+    for i in range(N):
+        print(board[i])
 
     #뱀 방향전환
     for i in range(L):
@@ -66,3 +80,4 @@ while(True):
     time = time + 1
 
 print(time)
+
