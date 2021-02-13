9754511f219ee12495573ebb6cc8635223414679
HR_SONG <yooatom2200@gmail.com>
2021-02-13 09:54:27 +0900

    중간저장

--- 백준_14502.py
+++ 백준_14502.py
@@ -1,5 +1,7 @@
 #백준 14502 연구소
-#지도 크기 입력
+#지도 입력
 N, M = map(int, input().split())
 area = [[0 for _ in range (N)] for _ in range (M)]
-print(area)
+for i in range (N):
+    area[i] = map(int, input().split())
+
