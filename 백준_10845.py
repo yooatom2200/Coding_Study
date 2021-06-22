97863104be216e09c8174016c1aa437aff222cf4
HR_SONG <yooatom2200@gmail.com>
2021-06-22 17:23:43 +0900

    백준 10845번 완료

--- 백준_10845.py
+++ 백준_10845.py
@@ -10,55 +10,53 @@ empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
 front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 '''
+import sys
+
 class Queue:
     def __init__(self):#초기화
         self.item = []
-    
+
     def enqueue(self, a):#삽입
         self.item.append(a)
 
     def dequeue(self):#삭제
-        if len(self.item) < 1:
-            print("error")
-            return
-        data = self.item[0]
-        del self.item[0]
-        return data
-    
+        if len(self.item) != 0:
+            return self.item.pop(0)
+        else:
+            return -1
+
     def is_empty(self):#큐 상태
         if len(self.item) > 0:
             return 0
         else:
             return 1
 
-    def front(self):
-        if self.is_empty == 1:
-            return -1
-        else:
+    def front(self):#맨앞 요소 출력
+        if len(self.item) != 0:
             return self.item[0]
-    
-    def back(self):
-        if self.is_empty == 1:
-            return -1
         else:
+            return -1
+    
+    def back(self):#맨뒤 요소 출력
+        if len(self.item) != 0:
             return self.item[-1]
-
+        else:
+            return -1
 
 test = Queue()
-line = input()
 
-for i in range(int(line)):
-    req = input().split(' ')
+for _ in range(int(sys.stdin.readline())):
+    req = sys.stdin.readline().split()
 
     if req[0] == "push":
         test.enqueue(req[1])
     elif req[0] == "pop":
         print(test.dequeue())
     elif req[0] == "size":
-        print(test.item.index)
+        print(len(test.item))
     elif req[0] == "empty":
         print(test.is_empty())
     elif req[0] == "front":
-        print(test.front)
+        print(test.front())
     elif req[0] == "back":
-        print(test.back)
+        print(test.back())
