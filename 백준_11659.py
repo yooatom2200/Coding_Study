a2b457dbef225b365e217d12b74b741bedf6901b
HR_SONG <yooatom2200@gmail.com>
2021-07-03 14:33:35 +0900

    백준 11659번 중간

--- 백준_11659.py
+++ 백준_11659.py
@@ -11,6 +11,6 @@ print(data)
 for _ in range(m):
     x, y = map(int, sys.stdin.readline().split())
     out = 0
-    for i in range(x, y):
-        out += int(data[i-1])
+    for i in range(x, y+1):#마지막 숫자까지 포함해서 더해야하므로 +1해줌
+        out += int(data[i-1])#배열순번 -1
     print(out)
