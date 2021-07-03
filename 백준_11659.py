b47d66e261d4febaf2edbd765909fd7040203fb7
HR_SONG <yooatom2200@gmail.com>
2021-07-03 14:31:17 +0900

    백준 11659번 시작

---
+++ 백준_11659.py
@@ -0,0 +1,16 @@
+'''
+구간합 구하기 4
+그냥 순서대로 입력받아서
+조건에맞게 합쳐주면 끝
+'''
+import sys
+n, m = map(int, sys.stdin.readline().split())
+data = sys.stdin.readline().split()
+print(data)
+
+for _ in range(m):
+    x, y = map(int, sys.stdin.readline().split())
+    out = 0
+    for i in range(x, y):
+        out += int(data[i-1])
+    print(out)
