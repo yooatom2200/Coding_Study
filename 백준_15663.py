acb288ac3c95cc15bd54ea9d17938a740cf30e2c
HR_SONG <yooatom2200@gmail.com>
2021-07-25 01:03:26 +0900

    백준 15663번 n과m문제 중간완성

--- 백준_15663.py
+++ 백준_15663.py
@@ -1,21 +1,27 @@
 '''
 백준 n과 m 문제
+중복수열을 걸러주는것이 중요하다
+트리처럼 구성해서 따라가다 보면 정렬방법의 해답이 보임
 '''
+
 import sys
 n, m = map(int, sys.stdin.readline().split())
-q = list(map(int, sys.stdin.readline().split())).sort()
+q = list(map(int, sys.stdin.readline().split()))
+q.sort()#sort 는 따로 입력해줘야함
+
 check = [0] * n
 out = []
-final = []
 
 def test(dept):
     if dept == m :
         tmp = ' '.join(map(str, out))
-        if tmp not in final:
-            final.append(tmp)
-
+        print(tmp)
+        return
+    
+    before = 0 #이전값을 비교해서 중복값을 걸러준다
     for i in range(n):
-        if check[i] == 0:
+        if check[i] == 0 and before != q[i]:
+            before = q[i]
             out.append(q[i])
             check[i] = 1
             test(dept+1)
@@ -23,5 +29,3 @@ def test(dept):
             check[i] = 0
         
 test(0)
-for i in final:
-    print(i)
