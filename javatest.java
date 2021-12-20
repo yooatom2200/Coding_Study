e813d0d86948d4c5ff1bfe2864e971700aeeca99
yooatom2200 <yooatom2200@gmail.com>
2021-12-20 23:58:44 +0900

    백준 1000번 예제 실행

--- javatest.java
+++ javatest.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class javatest{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
     }
 }
