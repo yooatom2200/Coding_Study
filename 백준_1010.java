33c58085a14b9e12607a85ba59c3d335203876bd
yooatom2200 <yooatom2200@gmail.com>
2021-12-21 17:47:23 +0900

    백준 1010번 시작

---
+++ 백준_1010.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class 백준_1010 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine());
+    }
+}
