d4104be781063fbd0b2f1aa528fb5cd98de36faa
yooatom2200 <yooatom2200@gmail.com>
2021-12-23 23:41:46 +0900

    백준 9012 입력값까지

--- 백준_9012.java
+++ 백준_9012.java
@@ -5,9 +5,14 @@ import java.io.InputStreamReader;
 public class 백준_9012 {
     public static void main(String[] args) throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int testcase  = Integer.parseInt(bf.readLine());
-        for(int i = 0; i < testcase; i++){
-
+        int count  = Integer.parseInt(bf.readLine());
+        String testcase[] = new String[count];
+        for(int i = 0; i < count; i++){
+            testcase[i] = bf.readLine();
+        }
+        System.out.println("출력시작");
+        for(int i = 0; i < count; i++){
+            System.out.println(testcase[i]);
         }
     }
 }
