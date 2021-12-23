1e64bab27ce5019a121342cfb49ae996fc192c95
yooatom2200 <yooatom2200@gmail.com>
2021-12-23 23:34:08 +0900

    백준 9012 시작

---
+++ 백준_9012.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준_9012 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int testcase  = Integer.parseInt(bf.readLine());
+        for(int i = 0; i < testcase; i++){
+
+        }
+    }
+}
