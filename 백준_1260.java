b1c3adf6c8d8cf042baaa5befc4fe0e3b0024e2b
yooatom2200 <yooatom2200@gmail.com>
2021-12-28 23:00:55 +0900

    백준 1260번 중간완성

--- 백준_1260.java
+++ 백준_1260.java
@@ -1,18 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-public class 백준_1260 {
-    static int[][] check;
-    public static void main(String[] args) throws IOException{
-        
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String testcase = bf.readLine();
-        String[] NMV = testcase.split(" ");
-        int N = Integer.parseInt(NMV[0]);
-        int M = Integer.parseInt(NMV[1]);
-        int V = Integer.parseInt(NMV[2]);
-        check = new int[10][10];
-    }
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+public class 백준_1260 {
+    public static int[][] arr;
+	public static boolean[] visited;
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        
+		int point = scan.nextInt();
+		int line = scan.nextInt();
+		int start = scan.nextInt();
+		
+		arr = new int[point+1][point+1];
+		
+		for(int i=1;i<=line;i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			arr[a][b] = 1;
+			arr[b][a] = 1;
+		}
+        //DFS
+		visited = new boolean[point+1];
+		dfs(start); 
+		
+		System.out.println();
+        
+        //BFS
+		visited = new boolean[point+1];
+		bfs(start); 
+
+		
+	}
+	public static void dfs(int start) {
+		visited[start] = true;
+		System.out.print(start+ " ");
+		
+		if(start == arr.length) {
+			return;
+		}
+
+		for(int a=1;a<arr.length;a++) {
+			if(arr[start][a] == 1 && visited[a] == false) {
+				dfs(a);
+			}
+		}
+			
+	}
+	public static void bfs(int start) {
+		Queue<Integer> que = new LinkedList<Integer>(); 
+		
+		que.add(start);
+		visited[start] = true;
+ 		System.out.print(start+ " ");
+		
+		while(!que.isEmpty()) {
+			
+			int temp = que.peek();
+			que.poll();
+			for(int i=1; i<arr.length;i++) {
+				if(arr[temp][i] ==1 && visited[i] == false) {
+					que.add(i);
+					visited[i] = true;
+					System.out.print(i+ " ");
+				}
+			}
+		}
+	}
+    
 }
