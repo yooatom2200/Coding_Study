c6ccb2c601f8d10924ec8d8530fcff217ed3aefb
yooatom2200 <yooatom2200@gmail.com>
2022-01-01 23:24:07 +0900

    백준 1003번 입력카운트 완료

--- 백준_1003.java
+++ 백준_1003.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class 백준_1003 {
     static int tcase = 0;
+    static int zCount = 0;
+    static int oCount = 0;
     static int fibonacci(int a){
         if (a == 0){
+            zCount += 1;
             return 0;
         }
         else if (a == 1){
+            oCount += 1;
             return 1;
         }
         else{
@@ -16,10 +20,12 @@ public class 백준_1003 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         tcase = sc.nextInt();
-        
-        for(int i = 0; i < tcase; i++){
 
+        for(int i = 0; i < tcase; i++){
+            fibonacci(sc.nextInt());
+            System.out.printf("%d %d\n", zCount, oCount);
+            zCount = 0;
+            oCount = 0;
         }
     }
-    
 }
