5c553435a5c05bc5beb0722b7a7083c2e37320b6
yooatom2200 <yooatom2200@gmail.com>
2022-01-03 19:17:36 +0900

    백준 1043번 시작

---
+++ 백준_1043.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+many = list(map(int,input().split()))[1:]
+partyCount = 0
+
+for _ in range(M):
+    inData = list(map(int,input().split()))
+    inData = inData[1:]
+    checkIn = 0
+
+    for i in inData:
+        if i in many:
+            checkIn = 1
+
+    if checkIn == 1:
+        for i in inData:
+            many.append(i)
+            many = list(set(many))
+    else:
+        partyCount += 1
+
+print(many)
+print(partyCount)
