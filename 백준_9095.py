dc8964f20848fc4981b801f185249b164439e955
yooatom2200 <yooatom2200@gmail.com>
2022-01-05 21:12:06 +0900

    백준 9095번 시작

---
+++ 백준_9095.py
@@ -0,0 +1,14 @@
+def fib(a):
+    if a == 1:
+        return 1
+    elif a == 2:
+        return 2
+    elif a == 3:
+        return 4
+    elif triple[a] != 0:
+        return triple[a]
+    triple[a] = fib(a-1) + fib(a-2) + fib(a-3)
+    return triple[a]
+
+triple = [0] * 11
+print(fib(int(input())))
