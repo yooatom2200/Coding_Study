994af33e014c5b80ee7646fdbcabcd50f9e908ed
yooatom2200 <yooatom2200@gmail.com>
2022-01-05 21:56:37 +0900

    백준 2293번 시작

---
+++ 백준_2293.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+coin = []
+num = [0] * (b+1)
+num[0] = 1
+for _ in a:
+    coin.append(int(input()))
+
+for i in coin:
+    for j in range(i, b+1):
+        num[j] += num[j-i]
+
+print(num[b])
