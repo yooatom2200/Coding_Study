a14c40b46a319de4c9b73c2887d3c94210e79bcf
yooatom2200 <yooatom2200@gmail.com>
2022-01-05 20:54:51 +0900

    백준 2748번 완성

--- 백준_2748.py
+++ 백준_2748.py
@@ -1,7 +1,6 @@
 # DP 다이나믹 프로그래밍
 # 한번 계산된건 반복 계산하지 않는다
 
-#num = input()
 def fib(a):
     if a == 0 :
         return 0
@@ -10,8 +9,9 @@ def fib(a):
     elif calc_data[a] != 0 :
         return calc_data[a]
     else :
-        calc_data[a] = calc_data[a-1] + calc_data[a-2]
+        calc_data[a] = fib(a-1) + fib(a-2)
         return calc_data[a]
 
-calc_data = [0] * 90
-print(fib(input()))
+num = int(input())
+calc_data = [0] * (num+1)
+print(fib(num))
