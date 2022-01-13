1f821fcbad15aeda5523feb60cffbe64b968b185
yooatom2200 <yooatom2200@gmail.com>
2022-01-13 21:08:26 +0900

    백준 1399 부분완료

--- 백준_1339.py
+++ 백준_1339.py
@@ -1,9 +1,19 @@
 n = int(input())
-numbers = []
-alphabet = []
-for i in range(n):
-    numbers.append(input())
+words = []
+for _ in range(n):
+    words.append(input())
 
-numbers.sort(key=len, reverse=True)
-for i in range(len(numbers), 0, -1):
-    if(numbers[i-1])
+count = {}
+for i in words:
+    wlen = len(i)
+    for j in i :
+        if j in count:
+            count[j] += 10 ** wlen
+        else:
+            count[j] = 10 ** wlen
+        wlen -= 1
+
+total = 0
+start = 9
+print(count)
+            
