c37b277ae2116a56666673531f414f360cf832f7
yooatom2200 <yooatom2200@gmail.com>
2022-01-13 21:10:42 +0900

    백준 1399 완성

--- 백준_1339.py
+++ 백준_1339.py
@@ -8,9 +8,9 @@ for i in words:
     wlen = len(i)
     for j in i :
         if j in count:
-            count[j] += 10 ** wlen
+            count[j] += 10 ** (wlen - 1)
         else:
-            count[j] = 10 ** wlen
+            count[j] = 10 ** (wlen - 1)
         wlen -= 1
 
 total = 0
