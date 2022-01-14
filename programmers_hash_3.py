aa0d8f8194c5c8ff94d987f9ba9a1c85bba637f4
yooatom2200 <yooatom2200@gmail.com>
2022-01-14 21:56:45 +0900

    프로그래머스 해시 3 통과

---
+++ programmers_hash_3.py
@@ -0,0 +1,18 @@
+"""
+dict함수 사용시 디폴트 출력은 key임.
+value 출력을 원하면 values()를 쓰고 키값쌍으로 받아올려면 items()를 써야함
+for나 in의 경우엔 key값 우선
+"""
+def solution(clothes):
+    category = {}
+    for i in clothes:
+        if i[1] in category:
+            category[i[1]] += 1
+        else:
+            category[i[1]] = 1
+            
+    rst = 1
+    for i in category.values():
+        rst *= (i + 1)
+        
+    return rst-1
