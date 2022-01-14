b45d4380f7040a5e31a8272af694d1f572bfa763
yooatom2200 <yooatom2200@gmail.com>
2022-01-14 21:01:43 +0900

    프로그래머스 해시 1 100점"

--- programmers_hash_1.py
+++ programmers_hash_1.py
@@ -1,7 +1,8 @@
 def solution(participant, completion):
-    for i in completion:
-        if i in participant:
-            participant.remove(i)
-            
-    answer = str(participant[0])
-    return answer
+    participant.sort()
+    completion.sort()
+    for i, j in zip(participant, completion):
+        print(participant)
+        if i != j:
+            return i
+    return participant.pop()
