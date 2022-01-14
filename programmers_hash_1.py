8f8970df8ed82b5aa88fc707bf365931a349c230
yooatom2200 <yooatom2200@gmail.com>
2022-01-14 20:42:53 +0900

    프로그래머스 해시 1 성공

--- programmers_hash_1.py
+++ programmers_hash_1.py
@@ -1,3 +1,7 @@
 def solution(participant, completion):
-    answer = ''
+    for i in completion:
+        if i in participant:
+            participant.remove(i)
+            
+    answer = str(participant[0])
     return answer
