087bf1ded65b471d8692bd4286f54d0b7cdd869a
yooatom2200 <yooatom2200@gmail.com>
2022-01-14 21:03:39 +0900

    프로그래머스 해시 1 효율성 통과

--- programmers_hash_1.py
+++ programmers_hash_1.py
@@ -1,3 +1,8 @@
+"""
+효율성 통과를 위해 두 배열을 정렬한 뒤
+zip으로 하나씩 호출하고 비교후 다른게 있으면 p에서 출력
+전부 통과하면 마지막 인원이 검거된것이므로 마지막사람 추출을 위해 pop 진행
+"""
 def solution(participant, completion):
     participant.sort()
     completion.sort()
