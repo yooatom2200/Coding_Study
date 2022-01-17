62f73d1ed0472289f976e78b301da70cfe75894c
yooatom2200 <yooatom2200@gmail.com>
2022-01-17 20:32:49 +0900

    스택큐 4번 효율성 통과

--- programmers_stackqueue_4.py
+++ programmers_stackqueue_4.py
@@ -1,16 +1,21 @@
 """
 파이썬 리스트를 그냥 복사할 경우 깊은복사가 진행된다
 주소까지 싹 공유되기에 얕은복사를 원할경우 .copy()를 사용하면 된다
+효율성 테스트 통과 필요 -> deque 사용
 """
+from collections import deque
+
 def solution(prices):
-    answer = [0] * len(prices)
-    idx = 0
-    while prices:
-        a = prices.pop(0)
-        for i in prices:
-            answer[idx] += 1
+    p_deq = deque(prices)
+    answer = []
+    
+    while p_deq:
+        a = p_deq.popleft()
+        tmp = 0
+        for i in p_deq:
+            tmp += 1
             if a > i:
                 break
-        idx += 1
+        answer.append(tmp)
                 
     return answer
