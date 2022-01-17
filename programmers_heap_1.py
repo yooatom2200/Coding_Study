288b5eaad50ab243c55a5a1a74a559631e779293
yooatom2200 <yooatom2200@gmail.com>
2022-01-17 21:14:39 +0900

    힙 1번 효율성 통과

--- programmers_heap_1.py
+++ programmers_heap_1.py
@@ -1,13 +1,21 @@
 """
 효율성 검사 씹창남.. 뭐지?
+heapq를 쓰면 됨
+히프는 자동정렬되서 정렬시간을 단축 할 수 있음
+ㅔㅐㅔ
 """
+import heapq
+
 def solution(scoville, K):
+    heap = []
+    for i in scoville:
+        heapq.heappush(heap, i)
+
     answer = 0
-    while min(scoville) < K:
+    while heap[0] < K:
         answer += 1
-        scoville.sort()
-        if len(scoville) < 2:
+        if len(heap) < 2:
             return -1
-        scoville.append(scoville.pop(0) + (scoville.pop(0) * 2))
+        heapq.heappush(heap,heapq.heappop(heap) + heapq.heappop(heap) * 2)
         
     return answer
