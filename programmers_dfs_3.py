a40bfa260ea04c423b637400720e6dc7b084fea9
yooatom2200 <yooatom2200@gmail.com>
2022-01-22 15:39:45 +0900

    DFS 3번 완성

--- programmers_dfs_3.py
+++ programmers_dfs_3.py
@@ -1,20 +1,13 @@
-"""
-bfs로 풀어야된다... dfs 재귀로 하면 넘나 깊어지나?
-또 나왔다... 깊은복사 조심하기
-"""
-def bfs(begin, target, words):
+def solution(begin, target, words):
     q = [begin]
     visit = [0] * len(words)
     count = 0
+    
     while True:
-        if target in q:
-            return count
-        elif sum(visit) == len(words):
-            return 0
+        count += 1
         
-        tmp_q = [i for i in q]
-        q = []
-        for tmp_w in tmp_q:
+        tmp_q = []
+        for tmp_w in q:
             if tmp_w == target:
                 return count
             tmp_list = list(tmp_w)
@@ -25,15 +18,13 @@ def bfs(begin, target, words):
                     if i == j :
                         same_count += 1
                 if same_count == len(target) - 1 and visit[word_index] == 0:
+                    if words[word_index] == target:
+                        return count
                     visit[word_index] = 1
-                    q.append(words[word_index])
-        count += 1
-
-def solution(begin, target, words):
-    return bfs(begin, target, words)
- 
-begin = "hit"
-target = "cog"
-words = ["hot", "dot", "dog", "lot", "log", "cog"]
-a = solution(begin, target, words)
-print(a)
+                    tmp_q.append(words[word_index])
+                    
+        if not tmp_q :
+            return 0
+        q = tmp_q
+        
+    return count
