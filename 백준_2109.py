4cf9cbdbc9f1d46b101c22be59a1631b29c1a3bd
yooatom2200 <yooatom2200@gmail.com>
2022-01-25 23:26:59 +0900

    백준 2109 잘못된 풀이

--- 백준_2109.py
+++ 백준_2109.py
@@ -4,16 +4,14 @@ for _ in range(N):
     a, b = map(int, input().split())
     schedule.append([a, b])
 
-schedule.sort(key = lambda x : x[0], reverse=True)
 schedule.sort(key = lambda x : x[1])
+answer, max_day = schedule.pop(-1)
+count = 1
+schedule.sort(key = lambda x : x[0], reverse=True)
+while schedule:
+    if count == max_day:
+        break
+    answer += schedule.pop(0)[0]
+    count += 1
 
-
-answer = 0
-day = 0
-
-for i in schedule:
-    if i[1] > day:
-        day = i[1]
-        answer += i[0]
-
-print(answer) if N != 0 else print(0)
+print(answer)
