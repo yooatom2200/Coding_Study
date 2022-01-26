9960dc448c88358af0038c965ce44c25eecd2aa6
yooatom2200 <yooatom2200@gmail.com>
2022-01-26 21:38:00 +0900

    백준 12015 시간초과

--- 백준_12015.py
+++ 백준_12015.py
@@ -1,5 +1,11 @@
 N = int(input())
 nums = list(map(int, input().split()))
+bigline = []
+while nums:
+    num = nums.pop(0)
+    if num in bigline:
+        continue
+    bigline.append(num)
 
-print(N)
-print(nums)
+print(len(bigline))
+    
