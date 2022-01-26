da35993078b1df16cac792b2c57a6353e9757408
yooatom2200 <yooatom2200@gmail.com>
2022-01-26 21:29:57 +0900

    백준 2109 주석

--- 백준_2109.py
+++ 백준_2109.py
@@ -1,17 +1,23 @@
+"""
+문제 조건을 항상 잘 읽기.
+문제만 잘 읽었어도 풀었을 문제.
+"""
+
 N = int(input())
 schedule = []
 book = []
+answer = 0
 for _ in range(N):
     a, b = map(int, input().split())
     schedule.append([a, b])
 
 schedule.sort(key = lambda x : x[1])
-print(schedule)
 while schedule:
     pay, max_day = schedule.pop(0)
+    answer += pay
     book.append([pay, max_day])
     book.sort(key = lambda x : x[0])
     if len(book) > max_day:
-        print(book.pop(0), max_day)
+        answer -= book.pop(0)[0]
 
-print(book)
+print(answer)
