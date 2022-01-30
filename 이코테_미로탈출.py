94bb2935c2174c3c0e13aab205facd071072fc66
yooatom2200 <yooatom2200@gmail.com>
2022-01-30 14:30:09 +0900

    이코테 미로찾기 솔

--- 이코테_미로탈출.py
+++ 이코테_미로탈출.py
@@ -1,17 +1,29 @@
 from collections import deque
-graph = []
-N, M = input().split()
-x, y = 0, 0
+matrix = []
+N, M = map(int,input().split())
 count = 0
 for _ in range(N):
-    graph.append(list(map(int,list(input()))))
+    matrix.append(list(map(int,list(input()))))
 
-def bfs():
+def bfs(x, y):
     queue = deque()
-    queue.append([x,y])
+    queue.append([x, y])
     mx = [1, -1, 0, 0]
     my = [0, 0, 1, -1]
     while queue:
-        qx, qy = queue.popleft()
+        pops = queue.popleft()
+        qx, qy = pops[0], pops[1]
         for i in range(4):
-            if qx + mx[i] < 0 or 
+            if qx + mx[i] < 0 or qx + mx[i] >= N or qy + my[i] < 0 or qy + my[i] >= M:
+                continue
+            if matrix[qx][qy] == 0:
+                continue
+            if matrix[qx+mx[i]][qy+my[i]] == 1:
+                queue.append([qx+mx[i], qy+my[i]])
+                matrix[qx+mx[i]][qy+my[i]] = matrix[qx][qy] + 1
+    
+    print(matrix)
+    return matrix[-1][-1]
+
+print(bfs(0,0))
+
