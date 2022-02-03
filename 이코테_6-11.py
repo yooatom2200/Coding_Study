4e4cf9d80f469bb730a6d760ac7890c4ab8d0728
yooatom2200 <yooatom2200@gmail.com>
2022-02-03 15:13:49 +0900

    이코테 6-11 솔

---
+++ 이코테_6-11.py
@@ -0,0 +1,11 @@
+N = int(input())
+data = []
+for _ in range(N):
+    ins = input().split()
+    data.append((ins[0], int(ins[1])))
+
+#data.sort(key = lambda x : x[1])
+data = sorted(data, key = lambda x : x[1])
+
+for i in data:
+    print(i[1], end=" ")
