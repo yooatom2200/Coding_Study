cf03ec01d83e5af39b69aca121c026eb3d5bcb4a
yooatom2200 <yooatom2200@gmail.com>
2022-02-07 20:04:08 +0900

    이코테 이진탐색 부품찾기 솔

---
+++ 이코테_부품찾기.py
@@ -0,0 +1,23 @@
+N = int(input())
+product = list(map(int, input().split()))
+product.sort()
+M = int(input())
+s_product = list(map(int, input().split()))
+
+def bs(start, end, search):
+    while start <= end:
+        mid = (start + end) // 2
+        if product[mid] == search:
+            return "yes"
+        elif product[mid] > search:
+            end = mid - 1
+        elif product[mid] < search:
+            start = mid + 1
+    return 0
+
+for i in s_product:
+    result = bs(0, N-1, i)
+    if result == 0:
+        print("NO", end=" ")
+    else:
+        print("YES", end=" ")
