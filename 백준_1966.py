ebdfa92737245f2ac19ab3ca3f2b1db2a2ca8062
yooatom2200 <yooatom2200@gmail.com>
2022-02-08 21:16:05 +0900

    백준 1966 예제통과

--- 백준_1966.py
+++ 백준_1966.py
@@ -1,6 +1,21 @@
 count = int(input())
-seq = 0
 for _ in range(count):
     note_count, note = map(int, input().split())
     notes = list(map(int, input().split()))
-    if notes[0] == max(notes):
+    notes_index = []
+    seq = 0
+
+    for i in range(len(notes)):
+        notes_index.append(i)
+
+    while notes:
+        if notes[0] == max(notes):
+            seq += 1
+            if notes_index[0] == note:
+                print(seq)
+                break
+            notes.pop(0)
+            notes_index.pop(0)
+        else:
+            notes.append(notes.pop(0))
+            notes_index.append(notes_index.pop(0))
